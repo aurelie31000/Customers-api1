d10cba7516a11f6f997f5cb6fb06760e92961daf
aurelie31000 <sihem-ange@hotmail.com>
2025-08-26 17:44:41 +0200

    Initial commit

---
+++ src/main/java/com/payetonkawa/customersapi/CustomersApiApplication.java
@@ -0,0 +1,11 @@
+package com.payetonkawa.customersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersApiApplication.class, args);
+    }
+}
---
+++ src/main/java/com/payetonkawa/customersapi/controllers/CustomerController.java
@@ -0,0 +1,56 @@
+package com.payetonkawa.customersapi.controllers;
+
+import com.payetonkawa.customersapi.models.Customer;
+import com.payetonkawa.customersapi.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
+        return customerRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long id, @RequestBody Customer customerDetails) {
+        return customerRepository.findById(id)
+                .map(customer -> {
+                    customer.setFirstName(customerDetails.getFirstName());
+                    customer.setLastName(customerDetails.getLastName());
+                    customer.setEmail(customerDetails.getEmail());
+                    return ResponseEntity.ok(customerRepository.save(customer));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+        if (customerRepository.existsById(id)) {
+            customerRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
